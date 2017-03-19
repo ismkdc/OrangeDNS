c70de42a84197f3a1f2ee759938731c11880c8d7
ismkdc <ism.kundakci@hotmail.com>
2017-03-19 13:16:24 +0300

    license text added

--- OrangeDNS.UI/Controllers/LogController.cs
+++ OrangeDNS.UI/Controllers/LogController.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿#region copyright
+// Copyright (c) 2017 All Rights Reserved
+// Author: İsmail Kundakcı
+// Author Website: www.ismailkundakci.com
+// Author Email: ism.kundakci@hotmail.com
+// Date: 19/03/2017 13:15:00
+// Description: OrangeDNS is a powerfull dns firewall solution written by C#
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Core.Objects;
 using System.Linq;
